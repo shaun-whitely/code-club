b91e420c14e6f3cbdc9f21634b691ea45e442473
Shaun Whitely <shaun.whitely@zendesk.com>
2022-06-03 15:47:49 +1000

    This should now find all possible mappings.

--- code_club.pl
+++ code_club.pl
@@ -1,7 +1,5 @@
 % This is a mostly working solution that greedily finds pairings between
-% students and teachers, based on some criteria in valid_pairing. It currently
-% doesn't find all solutions. For example, it won't allocate Wilma to any
-% teacher because all the teachers have been used up by the time we get there.
+% students and teachers, based on some criteria in valid_pairing.
 
 student(alice).
 student(bob).
@@ -43,14 +41,18 @@ all_teachers(Teachers) :- findall(Person, teacher(Person), Teachers).
 % ?- all_pairings(Pairings, US, UT).
 % Pairings = [alice-lorem, bob-ipsum],
 % US = UT, UT = [fred, wilma] ;
-% Pairings = [alice-ipsum, fred-lorem],
+% Pairings = [alice-lorem, fred-ipsum],
 % US = [bob, wilma],
 % UT = [fred, wilma] ;
-% false.
+% Pairings = [alice-lorem, wilma-ipsum],
+% US = [bob, fred],
+% UT = [fred, wilma] ;
+% etc.
 all_pairings(Pairings, UnallocatedStudents, UnallocatedTeachers) :-
   all_students(Students),
   all_teachers(Teachers),
-  student_teacher_pairings(Students, Teachers, Pairings, UnallocatedStudents, UnallocatedTeachers).
+  student_teacher_pairings(Students, Teachers, Pairings, UnallocatedStudents, UnallocatedTeachers),
+  sort(Pairings, Pairings).
 
 % if everyone has been allocated, there's nothing to do
 student_teacher_pairings([], [], [], [], []).
@@ -59,10 +61,11 @@ student_teacher_pairings([S|Students], [], [], [S|Students], []).
 % if there are no students, don't allocate anyone
 student_teacher_pairings([], [T|Teachers], [], [], [T|Teachers]).
 
-student_teacher_pairings([S|Students], Teachers, [S-T|Pairings], UnallocatedS, UnallocatedT) :-
-  valid_pairing(S, T),
+student_teacher_pairings(Students, Teachers, [S-T|Pairings], UnallocatedS, UnallocatedT) :-
+  select(S, Students, Students1),
   select(T, Teachers, Teachers1),
-  student_teacher_pairings(Students, Teachers1, Pairings, UnallocatedS, UnallocatedT).
+  valid_pairing(S, T),
+  student_teacher_pairings(Students1, Teachers1, Pairings, UnallocatedS, UnallocatedT).
 
 student_teacher_pairings([S|Students], Teachers, Pairings, [S|UnallocatedS], UnallocatedT) :-
   no_valid_pairings(S, Teachers),
